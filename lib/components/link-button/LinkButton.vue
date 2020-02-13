e51e5f4262ebdac4b2c5939ecb87c7fc2a9e8d5c
Robin Radic <rradic@hotmail.com>
2020-02-13 04:38:32 +0100

    link button, entry detail buttons, entry detail header tags

---
+++ lib/components/link-button/LinkButton.vue
@@ -0,0 +1,79 @@
+<template>
+    <a
+            class="el-button"
+            @click="handleClick"
+            :href="disabled ? null : href"
+            :class="[
+      type ? 'el-button--' + type : '',
+      buttonSize ? 'el-button--' + buttonSize : '',
+      {
+        'is-disabled': buttonDisabled,
+        'is-loading': loading,
+        'is-plain': plain,
+        'is-round': round,
+        'is-circle': circle
+      }
+    ]"
+    >
+        <i class="el-icon-loading" v-if="loading"></i>
+        <el-icon :name="icon" v-if="icon && !loading" />
+<!--        <i :class="icon" v-if="icon && !loading"></i>-->
+        <span v-if="$slots.default"><slot></slot></span>
+    </a>
+</template>
+<script>
+export default {
+    name: 'PyLinkButton',
+
+    inject: {
+        elForm    : {
+            default: ''
+        },
+        elFormItem: {
+            default: ''
+        }
+    },
+
+    props: {
+        href      : String,
+        type      : {
+            type   : String,
+            default: 'default'
+        },
+        size      : String,
+        icon      :String,
+        nativeType: {
+            type   : String,
+            default: 'button'
+        },
+        loading   : Boolean,
+        disabled  : Boolean,
+        plain     : Boolean,
+        autofocus : Boolean,
+        round     : Boolean,
+        circle    : Boolean
+    },
+
+    computed: {
+        _elFormItemSize() {
+            return (this.elFormItem || {}).elFormItemSize;
+        },
+        buttonSize() {
+            return this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
+        },
+        buttonDisabled() {
+            return this.disabled || (this.elForm || {}).disabled;
+        }
+    },
+
+    methods: {
+        handleClick(event) {
+            if ( !this.disabled ) {
+                if ( !this.href ) {
+                    this.$emit('click', event);
+                }
+            }
+        }
+    }
+};
+</script>
---
+++ lib/components/link-button/index.ts
@@ -0,0 +1,5 @@
+import LinkButton from './LinkButton.vue'
+
+export {
+    LinkButton
+}
