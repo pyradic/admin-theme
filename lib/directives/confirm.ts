ddb2a256bc960cb01f5075922060ddb35852ae5f
Robin Radic <rradic@hotmail.com>
2020-03-10 00:25:37 +0100

    CRVS-2 #time 4h alle hulpvrager klant gerelateerde functionaliteit werkt, behalve hulpvragen matchen

--- lib/directives/confirm.ts
+++ lib/directives/confirm.ts
@@ -1,22 +1,31 @@
 import { DirectiveOptions } from 'vue';
 
-import { MessageBox } from 'element-ui';
+import { MessageBox }          from 'element-ui';
+import { ElMessageBoxOptions } from 'element-ui/types/message-box';
 
 export const Confirm: DirectiveOptions = {
     bind            : (el, binding, vnode, oldVnode) => {
         const { name, arg, modifiers, expression, oldArg, oldValue, value } = binding;
         console.log({ name, arg, modifiers, expression, oldArg, oldValue, value });
+
+        let options: ElMessageBoxOptions = {
+            message: 'Weet u zeker dat u deze actie wilt uitvoeren?',
+            title  : 'Bevestigen',
+            type   : 'warning',
+        };
+        if ( typeof value === 'object' ) {
+            options = { ...options, ...value };
+        } else if(typeof value === 'string'){
+            options.message = value;
+        }
+
         el[ '__confirm_click' ] = async (event: MouseEvent) => {
             event.preventDefault();
-            MessageBox.confirm(value.question || 'Weet u zeker dat u deze actie wilt uitvoeren?', value.title || 'Bevestigen', {
-                type: 'warning',
-            }).then(() => {
+            MessageBox.confirm(options.message as string, options).then(() => {
                 const url = el.getAttribute(arg || 'href');
                 console.log('Delete it', { arg, url });
                 window.location.href = url;
-            }).catch(() => {
-                console.log('do nothing');
-            });
+            }).catch(() => console.log('do nothing'));
         };
         el.addEventListener('click', el[ '__confirm_click' ]);
     },
